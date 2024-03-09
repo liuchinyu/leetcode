74c24122eaf3a836e1372c0702673958f05c617c
Liu <jerryliu@gmail.com>
2024-03-09 12:39:42 +0800

    third version

--- Easy_1295.js
+++ Easy_1295.js
@@ -2,13 +2,8 @@ let nums = [555, 901, 482, 1771];
 let evenNumber = 0;
 
 nums.forEach((num) => {
-  let times = 0;
-  while (num > 0) {
-    times++;
-    num = Math.floor(num / 10, 0);
-    //預防小數
-  }
-  if (times % 2 == 0) {
+  let checkEven = num.toString().length;
+  if (checkEven % 2 == 0) {
     evenNumber++;
   }
 });
